60720b269d43086c4a43240977eefe8ac885be2c
Gabriel Queiroz <gabrieljvnq@gmail.com>
2019-05-24 13:28:17 -0300

    Working on number processing.

--- money.go
+++ money.go
@@ -1,9 +1,80 @@
 package r18n
 
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
 // Format: {[thousand separator][decimal separator][digits after separator]}
 // Ex:  {.} 1.000
 // Ex:  {.,2} 1.000,00
 // Ex:  {,.2} 1,000.00
-func FormatMoney(fmt string, amount int) string {
-	return ""
+func FormatNumber(fmt string, amount int) string {
+	ans := strings.Builder{}
+	tmp := strings.Builder{}
+	mode := 0 // 0 - copying | 1 - escape | 2 - processing | 3 - finalizing
+	for i, c := range fmt {
+		println(mode, i, string(c))
+		switch mode {
+		case 0:
+			switch c {
+			case '\\':
+				mode = 1
+			case '{':
+				tmp.Reset()
+				mode = 2
+			default:
+				ans.WriteRune(c)
+			}
+		case 1:
+			ans.WriteRune(c)
+			mode = 0
+		case 2:
+			switch c {
+			case '}':
+				mode = 3
+			default:
+				tmp.WriteRune(c)
+			}
+		case 3:
+			mode = 0
+			ans.WriteString(actualFormatNumber(tmp.String(), amount))
+		default:
+			panic("unexpected mode in FormatMoney")
+		}
+	}
+	if mode == 3 {
+		ans.WriteString(actualFormatNumber(tmp.String(), amount))
+	}
+	println()
+
+	return ans.String()
+}
+
+func intPow(base, exp int) int {
+	ans := 1
+	for i := 0; i < exp; i++ {
+		ans *= base
+	}
+	return ans
+}
+
+func actualFormatNumber(fmt_ string, amount int) string {
+	sep_1000 := fmt_[0]
+	sep_dec := fmt_[1]
+	digits, err := strconv.Atoi(fmt_[2:])
+	if err != nil {
+		panic(err)
+	}
+
+	println(">", sep_1000, sep_dec, digits)
+
+	int_part := amount / intPow(10, digits)
+	frac_part := amount % intPow(10, digits)
+
+	println(">", int_part, frac_part)
+	ans := fmt.Sprintf("%d%c%d", int_part, sep_dec, frac_part)
+	println(">", ans)
+	return ans
 }
--- money_test.go
+++ money_test.go
@@ -6,8 +6,10 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestFormatMoney(t *testing.T) {
-	assert.Equal(t, "R$ 123.000,99", FormatMoney("R$ {.,2}", 12300099))
-	assert.Equal(t, "R$ 123.000,987", FormatMoney("R$ {.,3}", 123000987))
-	assert.Equal(t, "R$ 123.000", FormatMoney("R$ {.}", 123000))
+func TestFormatNumber(t *testing.T) {
+	assert.Equal(t, "R$ 123,99", FormatNumber("R$ {.,2}", 12399))
+	assert.Equal(t, "R$ 123.000,99", FormatNumber("R$ {.,2}", 12300099))
+	assert.Equal(t, "R$ 123.000,987", FormatNumber("R$ {.,3}", 123000987))
+	assert.Equal(t, "R$ 123.000", FormatNumber("R$ ç {.}", 123000))
+	assert.Equal(t, "R$ {}", FormatNumber("R$ \\{\\}", 0))
 }
